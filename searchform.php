a1a4caa90f9fe008d6760bd26c12788e81c30d66
Emmanuel Netter <enetter@cplusn.com>
2009-02-02 18:52:45 +0000

    Correction de la gestion de l'icône de recherche

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form method="get" id="searchform" class="left" action="<?php bloginfo('url'); ?>/">
 <div>
-<input type="text" value="Rechercher..." value="<?php the_search_query(); ?>" name="s" id="s" onfocus="this.value=''" title="Saisissez les termes que vous voulez chercher." /><input type="image" src="<?php echo get_option('home'); ?>/wp-content/themes/arthemia/images/magnify.gif" id="searchsubmit" />
+<input type="text" value="Rechercher..." value="<?php the_search_query(); ?>" name="s" id="s" onfocus="this.value=''" title="Saisissez les termes que vous voulez chercher." /><input type="image" src="<?php echo bloginfo('template_directory'); ?>/images/magnify.gif" id="searchsubmit" />
 
 </div>
 </form>
